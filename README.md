845101e26784d95553e9a356f5633b712f2cf9dc
moazmar <moazmar.tn@gmail.com>
2021-09-06 22:43:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# App0: The Default App---FlutterCourse
+ App0 : The Default App [FlutterCourse] [www.Flutter-Pro.com]
---
+++ android/app/src/main/kotlin/com/example/app0_the_default_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app0_the_default_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
